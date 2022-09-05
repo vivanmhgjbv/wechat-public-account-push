ed1cd2eb65d571e22fb8e21a4d133f17d889d34a
vivanmhgjbv <112840022+vivanmhgjbv@users.noreply.github.com>
2022-09-05 13:04:42 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,10 +4,10 @@ export const config = {
    */
 
   // 公众号APP_ID
-  APP_ID: "JAxHTsr2ZrqmjGKau_n-biQTvKJS2kQcaUoR1IDmVUk	",
+  APP_ID: "wx8611d3b1c43a8169",
 
   // 公众号APP_SECRET
-  APP_SECRET: "",
+  APP_SECRET: "c9cda86b958e4f3fc5aed3a34fe56216",
 
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
@@ -27,15 +27,15 @@ export const config = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "老婆0",
+      name: "宝贝",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "ohxOO6VskY2v40Kuf5ywIZVRM3puXhk",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "Wue80GrtY1nAi5a4tam4FDxZF59WNx9pwcBc06LMpANnu8",
       // 所在省份
-      province: "广东",
+      province: "陕西",
       // 所在城市
-      city: "湛江",
+      city: "延安",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://wangxinleo.cn"
     }, 
@@ -89,9 +89,9 @@ export const config = {
   /** 天气相关 */
 
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: "广东",
+  PROVINCE: "陕西",
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: "湛江",
+  CITY: "延安",
 
   /** 重要节日相关 */
 
@@ -103,7 +103,7 @@ export const config = {
    */
   FESTIVALS: [
     {"type": "生日", "name": "宝贝", "year": "2003", "date": "05-18"},
-    {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
+    {"type": "节日", "name": "认识", "year": "2018", "date": "09-03"},
     {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
     {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
   ],
